a2a275cb830ebf572cddc73fa7c1b39704f29cb8
Albert Juan Cortina <ajuancor@uoc.edu>
2022-09-29 17:56:46 +0200

    Part de llista

---
+++ pac3-vue-pokedex/src/views/LlistaPokemonView.vue
@@ -0,0 +1,53 @@
+<script setup>
+//import getPokemons from '@/services/getPokemons';
+import CardPokemon from '../components/CardPokemon.vue';
+</script>
+
+<template>
+  <main>
+    <CardPokemon v-bind:list="true" />
+  </main>
+</template>
+
+
+<script>
+  /*
+  export default {
+  name: "app",
+  data() {
+    return {
+      pokemons: null
+    }
+  },
+  created() {
+    getPokemons.getPokemons(0,905)
+    .then( response =>  {
+      this.pokemons = response.data;
+      console.log(response.data);
+    })
+
+    .catch ( error => {
+      console.log(error);
+    })
+  }*/
+    /*methods: {
+      getPokemons() {
+        // Método para obtener la lista de usuarios
+      },
+      postUsuario() {
+        // Método para crear un usuario
+      },
+      putUsuario() {
+        // Método para actualizar un usuario
+      },
+      deleteUsuario() {
+        // Método para borrar un usuario
+      },
+    },
+    mounted() {
+      this.getPokemons();
+    }
+  },
+}
+*/
+</script>
