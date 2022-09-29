ed7967aa87d81f2445e856ffcceed452ce616dbb
Albert Juan Cortina <ajuancor@uoc.edu>
2022-09-29 17:59:39 +0200

    Esborrar

--- pac3-vue-pokedex/src/views/CardDetailView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import CardPokemon from '../components/CardPokemon.vue';
-</script>
-
-<template>
-  <main>
-    <CardPokemon v-bind:list="false" v-bind:pokemon_id="this.$route.params.id" />
-  </main>
-</template>
