8964eb78b4180b55625218587d583d221d9f5f67
Mavesdragon <dmanas1997@uoc.edu>
2022-10-27 18:10:38 +0200

    millora header

--- pokedex-app/src/components/MenuPokemon.vue
+++ pokedex-app/src/components/MenuPokemon.vue
@@ -12,45 +12,45 @@ defineProps({
 </script>
 
 <template>
-  <div class="header">
-    <div class="container ">
-      <div class="row">
-        <div class="d-flex align-self-center justify-content-center">
+  
+  <div class="container ">
+    <div class="row">
+      <div class="d-flex align-self-center justify-content-center">
 
-          <h1 class="main-title">{{ msg }}</h1>
+        <h1 class="main-title">{{ msg }}</h1>
 
-          <div class="controls align-self-center">
-            <div class="content-control">
-              <label for="dark_theme">Dark</label>
-              <input type="radio" name="theme" id="dark_theme" value="dark" v-model="theme" @change="$emit('change-theme', $event,'dark')" />
-            </div>
-
-            <div class="content-control">
-              <label for="light_theme">Light</label>
-              <input type="radio" name="theme" id="light_theme" value="light" v-model="theme" @change="$emit('change-theme', $event,'light')" />
-            </div>
-
+        <div class="controls align-self-center">
+          <div class="content-control">
+            <label for="dark_theme">Dark</label>
+            <input type="radio" name="theme" id="dark_theme" value="dark" v-model="theme" @change="$emit('change-theme', $event,'dark')" />
           </div>
+
+          <div class="content-control">
+            <label for="light_theme">Light</label>
+            <input type="radio" name="theme" id="light_theme" value="light" v-model="theme" @change="$emit('change-theme', $event,'light')" />
+          </div>
+
         </div>
       </div>
     </div>
-    
-    <div class="main-menu">
-      <div class="container">
-        <nav class="navbar-menu">
-          <ul>
-            <li>
-              <!-- <RouterLink to="/">Home</RouterLink> -->
-              <RouterLink class="" to="/llista">Llista</RouterLink>
-              
-              
-              <RouterLink class="" to="/combat">Combat</RouterLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+  </div>
+  
+  <div class="main-menu">
+    <div class="container">
+      <nav class="navbar-menu">
+        <ul>
+          <li >
+            <!-- <RouterLink to="/">Home</RouterLink> -->
+            <RouterLink class="button" to="/llista">Llista</RouterLink>
+            
+            
+            <RouterLink class="button" to="/combat">Combat</RouterLink>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
+  
 </template>
 
 <style scoped>
